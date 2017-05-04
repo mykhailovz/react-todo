4abb05ed5e0a7fa40153316a283133cce8db5d92
Mykhailo <mykhailo.vz@gmail.com>
2017-05-04 06:39:41 -0400

    Add test suite for list component

---
+++ app/tests/components/Todo.test.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+var Todo = require('Todo');
+
+// for autocomplete only
+// require('mocha');
+
+describe('Todo', () => {
+    it('should exist', () => {
+        expect(Todo).toExist();
+    });
+});
