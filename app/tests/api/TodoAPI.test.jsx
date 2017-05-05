065f75cb2034b5803e6c55b8fa09a1c73600d7d0
Mykhailo <mykhailo.vz@gmail.com>
2017-05-05 03:16:53 -0400

    Add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -60,6 +60,50 @@ describe('TodoAPI', () => {
 
     });
 
+    describe('filteredTodos', () => {
+        var todos = [{
+            id: 1,
+            text: 'Some text here',
+            completed: true
+        }, {
+            id: 2,
+            text: 'Some text here',
+            completed: false
+        }, {
+            id: 3,
+            text: 'fomef text here',
+            completed: true
+        }];
+
+        it('should return all items if showCompleted is true', () => {
+            var filterTodos = TodoAPI.filteredTodos(todos, true, '');
+            expect(filterTodos.length).toBe(3);
+        });
+
+        it('should return items only if showCompleted is false',() => {
+            var filteredTodos = TodoAPI.filteredTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filteredTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by searchText', () => {
+            var filteredTodos = TodoAPI.filteredTodos(todos, true, 'some');
+            expect(filteredTodos.length).toBe(2);
+        });
+
+        it('should return all toods if searchText is empty', () => {
+            var filteredTodos = TodoAPI.filteredTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+
+    });
+
+
+
 });
 
 
