d81661df2702c8f772684bce406a22e00b56649f
Mykhailo <mykhailo.vz@gmail.com>
2017-05-08 00:41:35 -0400

    Add actions & action test

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
       './app/api'
     ],
     alias: {
-      applicationStyles: 'app/styles/app.scss'
+      applicationStyles: 'app/styles/app.scss',
+      actions: 'app/actions/action.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
